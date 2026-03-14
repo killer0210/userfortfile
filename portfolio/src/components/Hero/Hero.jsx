d7fbaf3707a5920aadcf2f991e5e1dfdbc343a83
killer0210 <132190909+killer0210@users.noreply.github.com>
2026-03-14 22:08:01 +0800

    update home and navbar

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -4,10 +4,10 @@ import './Hero.css'
 const Hero = () => {
     return (
         <div className="hero w-full h-full bg-white rounded-tr-[60px] rounded-bl-[60px] overflow-hidden relative">
-            <div className='px-24 py-12 bg-black inline-flex rounded-br-[60px] relative z-1'>
-                <h1 className='text-5xl font-bold text-white'>Hello, I'm</h1>
-                <div className='tag-right h-[70px] w-[70px]  bg-black z-10 absolute bottom-[-70px] left-0' />
-                <div className='tag-left h-[70px] w-[70px]  bg-black z-10 absolute top-0 right-[-70px]' />
+            <div className='px-8 py-8 xl:px-24 xl:py-12 h-auto lg:h-[15%] xl:h-[14%]   bg-black inline-flex items-center justify-center rounded-br-[60px] relative z-1'>
+                <h1 className='xl:text-5xl text-3xl font-bold text-white'>Hello, I'm</h1>
+                <div className='tag-right xl:h-[70px] xl:w-[70px] h-[50px] w-[50px]  bg-black z-10 absolute bottom-[-50px] left-0 xl:bottom-[-70px] xl:left-0' />
+                <div className='tag-left xl:h-[70px] xl:w-[70px] h-[50px] w-[50px]  bg-black z-10 absolute top-0 right-[-50px] xl:top-0 xl:right-[-70px]' />
             </div>
             <div className='hero-image w-full h-screen z-[-1] '></div>
             <div className='absolute bottom-0 left-0 px-24 py-12'>
@@ -18,39 +18,43 @@ const Hero = () => {
                     <button className='bg-black text-white px-4 py-2 rounded-md'>Contact Me</button>
                 </div> */}
             </div>
-            <div className='absolute bottom-0 right-0 px-24 py-12 z-55 bg-black text-white inline-flex gap-4 rounded-tl-[60px]'>
-                <ul className='flex flex-col gap-2'>
-                    <li>
-                        <a href="#" className='flex items-center gap-2'>
-                            <i className='fa-brands fa-linkedin text-white text-2xl'></i>
-                            <span className='text-white text-lg'>LinkedIn</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className='flex items-center gap-2'>
-                            <i className='fa-brands fa-github text-white text-2xl'></i>
-                            <span className='text-white text-lg'>GitHub</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className='flex items-center gap-2'>
-                            <i className='fa-brands fa-twitter text-white text-2xl'></i>
-                            <span className='text-white text-lg'>Twitter</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className='flex items-center gap-2'>
-                            <i className='fa-brands fa-facebook text-white text-2xl'></i>
-                            <span className='text-white text-lg'>Facebook</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className='flex items-center gap-2'>
-                            <i className='fa-brands fa-whatsapp text-white text-2xl'></i>
-                            <span className='text-white text-lg'>WhatsApp</span>
-                        </a>
-                    </li>
-                </ul>
+            <div className='absolute bottom-0 right-0 z-55 '>
+                <div className='px-24 py-12 bg-black text-white inline-flex gap-4 rounded-tl-[60px] relative '>
+                    <ul className='flex flex-col gap-2'>
+                        <li>
+                            <a href="#" className='flex items-center gap-2'>
+                                <i className='fa-brands fa-linkedin text-white text-2xl'></i>
+                                <span className='text-white text-lg'>LinkedIn</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className='flex items-center gap-2'>
+                                <i className='fa-brands fa-github text-white text-2xl'></i>
+                                <span className='text-white text-lg'>GitHub</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className='flex items-center gap-2'>
+                                <i className='fa-brands fa-twitter text-white text-2xl'></i>
+                                <span className='text-white text-lg'>Twitter</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className='flex items-center gap-2'>
+                                <i className='fa-brands fa-facebook text-white text-2xl'></i>
+                                <span className='text-white text-lg'>Facebook</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className='flex items-center gap-2'>
+                                <i className='fa-brands fa-whatsapp text-white text-2xl'></i>
+                                <span className='text-white text-lg'>WhatsApp</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div className='link-tag-left w-[70px] h-[70px] bg-black absolute bottom-0 left-[-70px]'></div>
+                    <div className='link-tag-right w-[70px] h-[70px] bg-black absolute top-[-70px] right-0'></div>
+                </div>
             </div>
         </div>
     )
--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,66 @@
 import React from 'react'
-import { Home, User, Folder } from 'lucide-react'
+import * as Lucide from 'lucide-react'
+import { NavLink, Link } from 'react-router-dom'
+
+const navItems = [
+  {
+    icon: Lucide.Home,
+    label: 'Home',
+    href: '/'
+  },
+  {
+    icon: Lucide.User,
+    label: 'About',
+    href: '/about'
+  },
+  {
+    icon: Lucide.Zap,
+    label: 'Projects',
+    href: '/projects'
+  },
+  {
+    icon: Lucide.Code2,
+    label: 'Skills',
+    href: '/skills'
+  },
+  {
+    icon: Lucide.Briefcase,
+    label: 'Experience',
+    href: '/experience'
+  }
+]
 
 const Navbar = () => {
+
   return (
-    <div className='flex gap-2 flex-col align-center h-full w-[25%] md:w-[15%] xl:w-[8%] '>
-      <h1 className='w-full aspect-square flex items-center justify-center text-4xl font-bold text-center rounded-full bg-white cursor-pointer  '>Ts.</h1>
-      <div className='relative h-full w-full rounded-full bg-white'>
-        <div className='absolute bottom-8  -translate-y-1/2 flex flex-col gap-4 items-center'>
-          <Home className='w-8 h-8 cursor-pointer' />
-          <User className='w-8 h-8 cursor-pointer' />
-          <Folder className='w-8 h-8 cursor-pointer' />
+    <>
+      <div className='hidden sm:flex gap-2 flex-col align-center h-full w-[25%] md:w-[15%] xl:w-[8%] '>
+        <h1 className='w-full aspect-square flex items-center justify-center text-4xl font-bold text-center rounded-full bg-white cursor-pointer  '>Ts.</h1>
+        <div className='relative h-full w-full rounded-full bg-white'>
+          <div className='nav-center'>
+            <div className='flex flex-col items-center justify-center'>
+              {navItems.map((item) => {
+                const IconComponent = item.icon;
+
+                return (
+                  <NavLink
+                    to={item.href}
+                    key={item.label}
+                    title={item.label}
+                    className={({ isActive }) =>
+                      `p-2 rounded-xl ${isActive ? 'bg-gradient-1 text-white' : 'text-gray-500'}`
+                    }
+                  >
+                    <IconComponent className="xl:w-12 xl:h-12 md:w-10 md:h-10 w-8 h-8 cursor-pointer " />
+                  </NavLink>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
-
-    </div>
+    </>
   )
 }
 
-export default Navbar 
+export default Navbar  
