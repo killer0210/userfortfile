afb1b7b515421e4476663621660049f207873813
killer0210 <132190909+killer0210@users.noreply.github.com>
2026-03-17 22:06:41 +0800

    update skill page

--- portfolio/src/pages/Skills/Skills.jsx
+++ portfolio/src/pages/Skills/Skills.jsx
@@ -10,29 +10,206 @@ const Skills = () => {
                     <div className='tag-right xl:h-[70px] xl:w-[70px] h-[50px] w-[50px]  bg-black z-10 absolute bottom-[-50px] left-0 xl:bottom-[-70px] xl:left-0' />
                     <div className='tag-left xl:h-[70px] xl:w-[70px] h-[50px] w-[50px]  bg-black z-10 absolute top-0 right-[-50px] xl:top-0 xl:right-[-70px]' />
                 </div>
-                <div>
-                    <div className='p-4'>
-                        <div className='flex items-center gap-2'>
-                            <div className='p-2 bg-orange-600 rounded-md inline-block'><Lucide.Monitor className='text-2xl text-white' /></div>
-                            <h2 className='text-xl font-bold'>Frontend Stack</h2>
-                        </div>
-                        <div className='mt-4 p-4 bg-gray-100 rounded-xl'>
-                            <div className='flex items-center gap-2 justify-between'>
-                                <div className=' rounded-md inline-flex items-center gap-2'>
-                                    <div className='p-2 bg-white rounded-md inline-block'>
-                                        <Lucide.Code2 className='text-2xl text-gray-500' />
+                <div className='w-full h-full flex items-center justify-center px-8'>
+                    <div className='flex md:flex-row flex-col gap-8 w-full mx-auto mb-12 '>
+                        <div className='p-4 bg-white rounded-xl shadow-lg flex-1'>
+                            <div className='flex items-center gap-2'>
+                                <div className='p-2 bg-orange-600 rounded-md inline-block'><Lucide.Monitor className='text-2xl text-white' /></div>
+                                <h2 className='text-xl font-bold'>Frontend Stack</h2>
+                            </div>
+                            <div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>React.js</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
                                     </div>
-                                    <h6 className='text-lg font-bold'>React.js</h6>
                                 </div>
-                                <ul className='flex gap-2'>
-                                    <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
-                                    <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
-                                    <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
-                                    <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
-                                    <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
-                                </ul>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>Tailwind CSS</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>JavaScript</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                        <div className='p-4 bg-white rounded-xl shadow-lg flex-1'>
+                            <div className='flex items-center gap-2'>
+                                <div className='p-2 bg-orange-600 rounded-md inline-block'><Lucide.Monitor className='text-2xl text-white' /></div>
+                                <h2 className='text-xl font-bold'>Backend Stack</h2>
+                            </div>
+                            <div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>Node.js</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>Express.js</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>MongoDB</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>SQL</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='p-4 bg-white rounded-xl shadow-lg flex-1'>
+                            <div className='flex items-center gap-2'>
+                                <div className='p-2 bg-orange-600 rounded-md inline-block'><Lucide.Monitor className='text-2xl text-white' /></div>
+                                <h2 className='text-xl font-bold'> Tools</h2>
+                            </div>
+                            <div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>React.js</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>Tailwind CSS</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                                <div className='mt-4 p-4 bg-gray-100 rounded-xl '>
+                                    <div className='flex items-center gap-2 justify-between col-span-1 bg-white rounded-xl p-2'>
+                                        <div className=' rounded-md inline-flex items-center gap-2'>
+                                            <div className='p-2 bg-white rounded-md inline-block'>
+                                                <Lucide.Code2 className='text-2xl text-gray-500' />
+                                            </div>
+                                            <h6 className='text-lg font-bold'>JavaScript</h6>
+                                        </div>
+                                        <ul className='flex gap-2'>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                            <li className='p-1 bg-orange-500 rounded-md inline-block'></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
